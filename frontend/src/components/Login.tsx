bcf03c6f3ccdd897a9ce4e9ffef78f93f7faa80b
tyler tran <tyler.tran88@yahoo.com>
2025-06-24 17:38:26 -0700

    added missing files

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+
+ const app_name = 'pocketprofessors.com'; //change to your app name
+ function buildPath(route:string) : string
+{
+if (process.env.NODE_ENV != 'development')
+{
+return 'http://' + app_name + ':5000/' + route;
+}
+else
+{
+return 'http://localhost:5000/' + route;
+}
+
+}
+
+function Login()
+{
+    const [message, setMessage] = useState('');
+    const [loginName, setLoginName] = React.useState('');
+    const [loginPassword, setPassword] = React.useState('');
+function handleSetLoginName(e: any): void
+    {
+        setLoginName(e.target.value);
+    }
+
+    function handleSetPassword(e: any): void
+    {
+        setPassword(e.target.value);
+    }
+async function doLogin(event:any) : Promise<void>
+{
+event.preventDefault();
+var obj = {login:loginName,password:loginPassword};
+var js = JSON.stringify(obj);
+try
+{
+const response = await fetch(buildPath('api/login'),
+{method:'POST',body:js,headers:{'Content-Type':
+'application/json'}});
+var res = JSON.parse(await response.text());
+if( res.id <= 0 )
+{
+setMessage('User/Password combination incorrect');
+}
+else
+{
+var user =
+{firstName:res.firstName,lastName:res.lastName,id:res.id}
+localStorage.setItem('user_data', JSON.stringify(user));
+setMessage('');
+window.location.href = '/cards';
+}
+}
+catch(error:any)
+{
+alert(error.toString());
+return;
+}
+};
+    
+
+    return(
+        <div id="loginDiv">
+            <span id="inner-title">PLEASE LOG IN</span><br />
+            Login: <input
+                type="text"
+                id="loginName"
+                placeholder="Username"
+                onChange={handleSetLoginName}
+            /><br />
+            Password: <input
+                type="password"
+                id="loginPassword"
+                placeholder="Password"
+                onChange={handleSetPassword}
+            /><br />
+            <input
+                type="submit"
+                id="loginButton"
+                className="buttons"
+                value="Do It"
+                onClick={doLogin}
+            />
+            <span id="loginResult">Output: {message}</span>
+        </div>
+    );
+};
+
+export default Login;
