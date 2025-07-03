693a45eb5189c7073820422bfbe59eb21517f35f
tyler tran <tyler.tran88@yahoo.com>
2025-07-03 11:17:10 -0700

    maybe fix

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -38,7 +38,7 @@ async function doRegister(event:any) : Promise<void>
 	var js = JSON.stringify(obj);
 	try
 	{
-		const response = await fetch(buildPath('api/register'),
+		const response = await fetch(buildPath('/api/register'),
 		{method:'POST',body:js,headers:{'Content-Type':
 		'application/json'}});
 		var res = JSON.parse(await response.text());
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+require('./routes/register');
 // =======================
 // Imports and Middleware
 // =======================
